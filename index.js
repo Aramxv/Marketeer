c15223b0e48f8e8677640b4e28d7d34823306625
Aramxv <reijraxyr@mgmail.com>
2020-07-20 13:35:15 +0800

    Newly Stocked Products Section

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     $("#top-sale .owl-carousel").owlCarousel({
         loop:true,
         nav:true,
-        dots:true,
+        dots:false,
         responsive:{
             // if the window width is 0 
             0:{
@@ -42,6 +42,27 @@ $(document).ready(function(){
         var filterValue=$(this).attr('data-filter');
         $grid.isotope({filter:filterValue});
     });
+
+    $("#newly-stocked .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            // if the window width is 0 
+            0:{
+                items:1 // show 1 item
+            },
+            // if the window width is 600
+            600:{
+                items:3 // show 3 items
+            },
+            // if the window width is 1000
+            1000:{
+                items:5 // show 5 items
+            }
+        }
+    });
+
 
     
 });
