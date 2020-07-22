f2bc3abec01f3901f552e3686ac492c413e6a2a4
Aramxv <reijraxyr@mgmail.com>
2020-07-22 15:46:33 +0800

    Product Details and Sections

--- index.js
+++ index.js
@@ -81,6 +81,27 @@ $(document).ready(function(){
         }
     });
 
+    // Product Quantity Section
+    let $qty_up=$(".qty .qty-up");
+    let $qty_down=$(".qty .qty-down");
+    let $input=$(".qty .qty_input");
 
+    // Click on qty up button
+    $qty_up.click(function(e){
+        if($input.val() >= 1 && $input.val() <= 11){
+            $input.val(function(i, oldval){
+                return ++oldval;    // Increase the text box value
+            })
+        }
+    });
+
+    // Click on qty down button
+    $qty_down.click(function(e){
+        if($input.val() > 1 && $input.val() <= 12){
+            $input.val(function(i, oldval){
+                return --oldval;    // Decrease the text box value
+            })
+        }
+    });
     
 });
